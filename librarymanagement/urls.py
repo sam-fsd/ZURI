901a6b08b3eb60c4430406470b475ea5ec1c6f7e
sam-fsd <kabirusam037@gmail.com>
2025-03-31 21:33:31 +0300

    update logout url

--- librarymanagement/urls.py
+++ librarymanagement/urls.py
@@ -36,8 +36,9 @@ urlpatterns = [
     path('studentlogin', LoginView.as_view(
         template_name='library/studentlogin.html')),
 
-    path('logout', LogoutView.as_view(template_name='library/index.html')),
-    path('afterlogin', views.afterlogin_view),
+    path('logout', views.custom_logout_view, name='logout'),
+    path(
+        'afterlogin', views.afterlogin_view),
 
     path('addbook', views.addbook_view),
     path('viewbook', views.viewbook_view),
